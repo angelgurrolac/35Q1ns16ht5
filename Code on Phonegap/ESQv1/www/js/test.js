2a9aa7ec4f45c622e3433c901ff49a50f73071f9
Guillermino <jgediaz@gmail.com>
2016-10-24 12:02:56 -0500

    ESQv1

---
+++ Code on Phonegap/ESQv1/www/js/test.js
@@ -0,0 +1,64 @@
+ var brick;
+    brick = "<div class='brick small'><div class='delete'>&times;</div></div>";
+
+ $(function() {
+
+   /*var sso = {
+    enableDrag: false
+  };
+
+  var ss = $(".gridly").shapeshift(sso);*/
+
+   $('.gridly').gridly('draggable', 'off');
+
+   if ($(window).width() <= 736) {
+     console.log($(window).width());
+     $('.gridly').gridly({
+         //base: 40, // px
+         gutter: 1, // px
+         columns: 4
+     });
+   }
+   else if ($(window).width() >= 738 && $(window).width() < 1024) {
+     console.log($(".gridly").width());
+       $('.gridly').gridly({
+           //base: 40, // px
+           gutter: 1, // px
+           columns: 12
+       });
+     }
+     else if ($(window).width() == 1024) {
+       console.log($(".gridly").width());
+         $('.gridly').gridly({
+             //base: 40, // px
+             gutter: 1, // px
+             columns: 16
+         });
+       }
+
+$(".gridly").on("press",function(){
+  $(".gridly > div").css("box-shadow", "0px 0px 25px rgb(204, 204, 204)");
+  $('.gridly').gridly('draggable', 'on');
+});
+
+
+ $(document).on("click", ".gridly .delete", function(event) {
+      var $this;
+      event.preventDefault();
+      event.stopPropagation();
+      $this = $(this);
+      $this.closest('.brick').remove();
+      return $('.gridly').gridly('layout');
+    });
+
+
+    $(document).on("click", "#disable", function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      //$('.gridly').append(brick);
+      //return $('.gridly').gridly();
+      $(".gridly > div").css("box-shadow", "");
+      $('.gridly').gridly('draggable', 'off');
+    });
+
+});
