50e12f8683b01fa482d8ec3a9d87a429514b5654
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2016-11-24 16:41:07 -0600

    phonegap serve modified this files
    Using phonegap 6.4.2.
    Platforms directory should be ignored by Git [1].
    
    1. https://github.com/phonegap/phonegap-app-developer/blob/master/.gitignore

--- Code on Phonegap/ESQv1S2/platforms/browser/www/js/widgets.js
+++ Code on Phonegap/ESQv1S2/platforms/browser/www/js/widgets.js
@@ -11,9 +11,11 @@ var Average, Interchange;
 //Origin
 var SameBank, OtherIssuer, DifferentATM;
 //Critical Failures
+var CriticalFailures=[];
 var COne, Ctwo, CThree, CFour, CFive;
+var COneValue, CtwoValue, CThreeValue, CFourValue, CFiveValue;
 /*Disposals-Available*/
-var Dis=[] , Ava=[], Instances=[];
+var Dis=[], Ava=[], Instances=[];
 //Transactions per Second
 var TransactionsSecond;
 //SLA-Violation
@@ -96,13 +98,18 @@ var atm;
    }
   });
 
-//Data Widgets
 
-$.ajax({
-type: "get",
-url: "js/DataAPIResponse.json",
-dataType:"json",
-success: function(data) {
+//Take Only the Numbers in the Array
+function Numbermatch(v){
+v=parseInt(v.match(/\d/g).join(""));
+return v;
+}
+
+//Take Only Letters in the Array
+function Valuematch(value){
+value= value.toString().match(/[A-Z\s]/g).join("");
+return value;
+}
 
 //Number Format function
 function SeparateNumber(val){
@@ -110,96 +117,8 @@ function SeparateNumber(val){
    val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
   }
     return val;
-  }
-
-//Working
-Active=data.kpis["sstob.atms"].data[0].values[0].data[0][1];
-Inactive=data.kpis["sstob.atms"].data[0].values[0].data[0][2];
-TotalATM=data.kpis["sstob.atms"].data[0].values[0].data[0][3];
-Active=SeparateNumber(Active);
-Inactive=SeparateNumber(Inactive);
-TotalATM=SeparateNumber(TotalATM);
-
-//Transactions
-for (var j = 0; j <= data.kpis["atmta.transactions"].data[0].values[0].data.length-1; j++){
-  TotalTransactions+=data.kpis["atmta.transactions"].data[0].values[0].data[j][1];
-  TotalTransactions=SeparateNumber(TotalTransactions);
-}
-//Transactions per Second
-TransactionsSecond=data.kpis["atmta.transactions"].data[0].values[0].data[0][1];
-//Approved Transactions
-Approved=data.kpis["atmta.transactions"].data[0].values[0].data[0][2];
-Denied=data.kpis["atmta.transactions"].data[0].values[0].data[0][3];
-Approved=SeparateNumber(Approved);
-Denied=SeparateNumber(Denied);
-//Response Time
-Average=data.kpis["atmta.transactions"].data[0].values[0].data[0][4]/300;
-Interchange=data.kpis["atmta.transactions"].data[0].values[0].data[0][5]/300;
-//Origin
-SameBank=data.kpis["atmta.transactions"].data[0].values[0].data[0][1];
-OtherIssuer=data.kpis["atmta.transactions"].data[1].values[0].data[0][1];
-DifferentATM=OtherIssuer-SameBank;
-SameBank=SeparateNumber(SameBank);
-OtherIssuer=SeparateNumber(OtherIssuer);
-DifferentATM=SeparateNumber(DifferentATM);
-
-//Critical Failures
-var CriticalFailures=[];
-CriticalFailures[0]=data.kpis["sstob.atmfaults"].data[0].values[0].data[0][1];
-CriticalFailures[0]+=" OUT OF SERVICE";
-CriticalFailures[1]=data.kpis["sstob.atmfaults"].data[0].values[0].data[0][2];
-CriticalFailures[1]+=" COMMUNICATION";
-CriticalFailures[2]=data.kpis["sstob.atmfaults"].data[0].values[0].data[0][3];
-CriticalFailures[2]+=" OUT OF CASH";
-CriticalFailures[3]=data.kpis["sstob.atmfaults"].data[0].values[0].data[0][4];
-CriticalFailures[3]+=" PRINTER";
-CriticalFailures[4]=data.kpis["sstob.atmfaults"].data[0].values[0].data[0][5];
-CriticalFailures[4]+=" OTHER";
-
-CriticalFailures.sort(function(v1,v2) {
-v1=parseInt(v1.match(/\d/g).join(''),10);
-v2=parseInt(v2.match(/\d/g).join(''), 10);
-    if (v1<v2)
-      return 1;
-    else
-      return 0;
-});
-
-COne=CriticalFailures[0];
-Ctwo=CriticalFailures[1];
-CThree=CriticalFailures[2];
-CFour=CriticalFailures[3];
-CFive=CriticalFailures[4];
-
-//Disposals-Available
-for (var i = 0; i < data.kpis["sstob.atmcash"].data.length; i++) {
-//Disposals
-Dis[i]=data.kpis["sstob.atmcash"].data[i].values[0].data[0][1];
-//Available
-Ava[i]=data.kpis["sstob.atmcash"].data[i].values[0].data[0][2];
-//Instance-Name
-Instances[i]=data.kpis["sstob.atmcash"].data[i].instanceName;
 }
 
-if (data.kpis["sstob.atmcash"].data.length===1) {
-  for (var k = 1; k <= 3; k++) {
-    Dis[k]="";
-    Ava[k]="";
-    Instances[k]="";
-  }
-}
-if (data.kpis["sstob.atmcash"].data.length===2) {
-  for (var h = 2; h <= 3; h++) {
-    Dis[h]="";
-    Ava[h]="";
-    Instances[h]="";
-  }
-}
-if (data.kpis["sstob.atmcash"].data.length===3) {
-    Dis[3]="";
-    Ava[3]="";
-    Instances[3]="";
-}
 //Function that allow simplify the numbers 
 function abbrNum(number, decPlaces) {
     var orig = number;
@@ -230,14 +149,119 @@ function abbrNum(number, decPlaces) {
   }
   return number;
 }
-D1= abbrNum(Dis[0], 4);
-D2= abbrNum(Dis[1], 4);
-D3= abbrNum(Dis[2], 4);
-D4= abbrNum(Dis[3], 4);
-A1= abbrNum(Ava[0], 4);
-A2= abbrNum(Ava[1], 4);
-A3= abbrNum(Ava[2], 4);
-A4= abbrNum(Ava[3], 4);
+
+//Data Widgets
+$.ajax({
+type: "get",
+url: "js/DataAPIResponse.json",
+dataType:"json",
+success: function(data) {
+
+//Working
+Active=data.kpis["sstob.atms"].data[0].values[0].data[0][1];
+Inactive=data.kpis["sstob.atms"].data[0].values[0].data[0][2];
+TotalATM=data.kpis["sstob.atms"].data[0].values[0].data[0][3];
+Active=SeparateNumber(Active);
+Inactive=SeparateNumber(Inactive);
+TotalATM=SeparateNumber(TotalATM);
+
+//Transactions
+for (var j = 0; j <= data.kpis["atmta.transactions"].data[0].values[0].data.length-1; j++){
+  TotalTransactions+=data.kpis["atmta.transactions"].data[0].values[0].data[j][1];
+  TotalTransactions=SeparateNumber(TotalTransactions);
+}
+//Transactions per Second
+TransactionsSecond=data.kpis["atmta.transactions"].data[0].values[0].data[0][1];
+//Approved Transactions
+Approved=data.kpis["atmta.transactions"].data[0].values[0].data[0][2];
+Denied=data.kpis["atmta.transactions"].data[0].values[0].data[0][3];
+Approved=SeparateNumber(Approved);
+Denied=SeparateNumber(Denied);
+//Response Time
+Average=data.kpis["atmta.transactions"].data[0].values[0].data[0][4]/300;
+Interchange=data.kpis["atmta.transactions"].data[0].values[0].data[0][5]/300;
+//Origin
+SameBank=data.kpis["atmta.transactions"].data[0].values[0].data[0][1];
+OtherIssuer=data.kpis["atmta.transactions"].data[1].values[0].data[0][1];
+//Origin-Operation
+DifferentATM=OtherIssuer-SameBank;
+//Add function
+SameBank=SeparateNumber(SameBank);
+OtherIssuer=SeparateNumber(OtherIssuer);
+DifferentATM=SeparateNumber(DifferentATM);
+
+//Critical Failures
+
+CriticalFailures[0]=data.kpis["sstob.atmfaults"].data[0].values[0].data[0][1];
+CriticalFailures[0]+=" OUT OF SERVICE";
+CriticalFailures[1]=data.kpis["sstob.atmfaults"].data[0].values[0].data[0][2];
+CriticalFailures[1]+=" COMMUNICATION";
+CriticalFailures[2]=data.kpis["sstob.atmfaults"].data[0].values[0].data[0][3];
+CriticalFailures[2]+=" OUT OF CASH";
+CriticalFailures[3]=data.kpis["sstob.atmfaults"].data[0].values[0].data[0][4];
+CriticalFailures[3]+=" PRINTER";
+CriticalFailures[4]=data.kpis["sstob.atmfaults"].data[0].values[0].data[0][5];
+CriticalFailures[4]+=" OTHER";
+
+CriticalFailures.sort(function(v1,v2) {
+v1=parseInt(v1.match(/\d/g).join(""));
+v2=parseInt(v2.match(/\d/g).join(""));
+    if (v1<v2)
+      return 1;
+    else
+      return 0;
+});
+
+COne=Numbermatch(CriticalFailures[0]);
+Ctwo=Numbermatch(CriticalFailures[1]);
+CThree=Numbermatch(CriticalFailures[2]);
+CFour=Numbermatch(CriticalFailures[3]);
+CFive=Numbermatch(CriticalFailures[4]);
+COneValue=Valuematch(CriticalFailures[0]);
+CtwoValue=Valuematch(CriticalFailures[1]);
+CThreeValue=Valuematch(CriticalFailures[2]);
+CFourValue=Valuematch(CriticalFailures[3]);
+CFiveValue=Valuematch(CriticalFailures[4]);
+
+//Disposals-Available
+for (var i = 0; i < data.kpis["sstob.atmcash"].data.length; i++) {
+//Disposals
+Dis[i]=data.kpis["sstob.atmcash"].data[i].values[0].data[0][1];
+//Available
+Ava[i]=data.kpis["sstob.atmcash"].data[i].values[0].data[0][2];
+//Instance-Name
+Instances[i]=data.kpis["sstob.atmcash"].data[i].instanceName;
+}
+
+if (data.kpis["sstob.atmcash"].data.length===1) {
+  for (var k = 1; k <= 3; k++) {
+    Dis[k]="";
+    Ava[k]="";
+    Instances[k]="";
+  }
+}
+if (data.kpis["sstob.atmcash"].data.length===2) {
+  for (var h = 2; h <= 3; h++) {
+    Dis[h]="";
+    Ava[h]="";
+    Instances[h]="";
+  }
+}
+if (data.kpis["sstob.atmcash"].data.length===3) {
+    Dis[3]="";
+    Ava[3]="";
+    Instances[3]="";
+}
+
+Dis[0]=abbrNum(Dis[0], 4);
+Dis[1]=abbrNum(Dis[1], 4);
+Dis[2]=abbrNum(Dis[2], 4);
+Dis[3]=abbrNum(Dis[3], 4);
+
+Ava[0]=abbrNum(Ava[0], 4);
+Ava[1]=abbrNum(Ava[1], 4);
+Ava[2]=abbrNum(Ava[2], 4);
+Ava[3]=abbrNum(Ava[3], 4);
 
 //SLA-Violation Information
 breached=data.kpis["sstob.sla"].data[0].values[0].data[0][1];
@@ -262,7 +286,6 @@ $.ajax({
   url: "js/preferences.json",
   dataType:"json",success: function (data) {
 
-for(var i=0, t=localStorage.length; i < t; i++) {
 $(data).each(function (index, data) {$('.widgets').html(
     "<div id="+data.widgets.working.id+" class='vertical col-lg-4 col-md-6 col-sm-6 col-xs-12 "+data.widgets.working.name+" 'style='position: relative; '>"+
       "<div class='left'>"+
@@ -318,8 +341,8 @@ $(data).each(function (index, data) {$('.widgets').html(
         "<div class='bottom'>"+
           "<a href='trends_approved.html'><span class='tendency-icon'></span></a>"+
               //Information
-              "<div class='widget-information'>"+                    
-                "<div class='col-lg-5 col-md-5 col-sm-5 col-xs-5  info-ATL'>"+
+              "<div class='widget-information'>"+
+              "<div class='col-lg-5 col-md-5 col-sm-5 col-xs-5  info-ATL'>"+
                  "<ul class='ul-widgets'>"+
                   "<li class='li-active'><span>APPROVED</span>"+
                     "<ul class='ul-infow'>"+
@@ -387,31 +410,31 @@ $(data).each(function (index, data) {$('.widgets').html(
         "<div class='widget-information widget-critical'>"+
             "<div class='col-lg-6 col-md-6 col-sm-6 col-xs-6 info-CF'>"+
               "<ul class='ul-widgets'>"+
-                    "<li class='li-one'><span class='percent-one'>"+COne.match(/\d/g).join('')+"%</span><span>" +COne.match(/[A-Z\s]/g).join('')+"</span></li>"+
+              "<li class='li-one'><span class='percent-one'>"+COne+"%</span><span>"+COneValue+"</span></li>"+
               "</ul>"+
             "</div>"+
 
              "<div class='col-lg-6 col-md-6 col-sm-6 col-xs-6 info-CF'>"+
               "<ul class='ul-widgets'>"+
-              "<li class='li-four'><span class='percent-four'>"+CFour.match(/\d/g).join('')+"%</span><span>" +CFour.match(/[A-Z\s]/g).join('')+"</span></li>"+   
+              "<li class='li-four'><span class='percent-four'>"+CFour+"%</span><span>" +CFourValue+"</span></li>"+
               "</ul>"+
             "</div>"+
 
             "<div class='col-lg-6 col-md-6 col-sm-6 col-xs-6 info-CF'>"+
               "<ul class='ul-widgets'>"+
-              "<li class='li-two'><span class='percent-two'>"+Ctwo.match(/\d/g).join('')+"%</span><span>"+ Ctwo.match(/[A-Z\s]/g).join('')+"</span></li>"+     
+              "<li class='li-two'><span class='percent-two'>"+Ctwo+"%</span><span>"+ CtwoValue+"</span></li>"+
               "</ul>"+
             "</div>"+
 
              "<div class='col-lg-6 col-md-6 col-sm-6 col-xs-6 info-CF'>"+
               "<ul class='ul-widgets'>"+
-              "<li class='li-five'><span class='percent-five'>"+CFive.match(/\d/g).join('')+"%</span><span>"+ CFive.match(/[A-Z\s]/g).join('')+"</span></li>"+
+              "<li class='li-five'><span class='percent-five'>"+CFive+"%</span><span>"+ CFiveValue+"</span></li>"+
               "</ul>"+
             "</div>"+
 
             "<div class='col-lg-6 col-md-6 col-sm-6 col-xs-6 info-CF'>"+
               "<ul class='ul-widgets'>"+
-              "<li class='li-three'><span class='percent-three'>"+CThree.match(/\d/g).join('')+"%</span><span>"+ CThree.match(/[A-Z\s]/g).join('')+"</span></li>"+ 
+              "<li class='li-three'><span class='percent-three'>"+CThree+"%</span><span>"+ CThreeValue+"</span></li>"+
               "</ul>"+
             "</div>"+
           "</div>"+
@@ -426,12 +449,12 @@ $(data).each(function (index, data) {$('.widgets').html(
         "<div id='disposals' class='tab-pane fade in active'>"+
           "<a href='notification-settings-disposals.html'><span class='notification-config'></span></a>"+
         //Information Disposals//
-              "<div class='widget-information'>"+    
-                "<div class='col-lg-5 col-md-5 col-sm-5 col-xs-5 info-DAL'>"+
+              "<div class='widget-information'>"+
+              "<div class='col-lg-5 col-md-5 col-sm-5 col-xs-5 info-DAL'>"+
                  "<ul class='ul-widgets'>"+
                   "<li><span class='accounting'>"+Instances[0]+"</span>"+
                     "<ul class='ul-infow'>"+
-                    "<li><span class='currency'>"+D1+"</span><span class='information-AI'></span></li>"+
+                    "<li><span class='currency'>"+Dis[0]+"</span><span class='information-AI'></span></li>"+
                     "</ul>"+
                   "</li>"+
                   "</ul>"+
@@ -440,7 +463,7 @@ $(data).each(function (index, data) {$('.widgets').html(
                  "<ul class='ul-widgets'>"+
                   "<li><span class='accounting'>"+Instances[1]+"</span>"+
                     "<ul class='ul-infow'>"+
-                    "<li><span class='currency'>"+D2+"</span><span class='information-AI'></span></li>"+
+                    "<li><span class='currency'>"+Dis[1]+"</span><span class='information-AI'></span></li>"+
                     "</ul>"+
                   "</li>"+
                 "</ul>"+
@@ -449,7 +472,7 @@ $(data).each(function (index, data) {$('.widgets').html(
                  "<ul class='ul-widgets'>"+
                   "<li><span class='accounting'>"+Instances[2]+"</span>"+
                     "<ul class='ul-infow'>"+
-                    "<li><span class='currency'>"+D3+"</span><span class='information-AI'></span></li>"+
+                    "<li><span class='currency'>"+Dis[2]+"</span><span class='information-AI'></span></li>"+
                     "</ul>"+
                   "</li>"+
                   "</ul>"+
@@ -458,7 +481,7 @@ $(data).each(function (index, data) {$('.widgets').html(
                  "<ul class='ul-widgets'>"+
                   "<li><span class='accounting'>"+Instances[3]+"</span>"+
                     "<ul class='ul-infow'>"+
-                    "<li><span class='currency'>"+D4+"</span><span class='information-AI'></span></li>"+
+                    "<li><span class='currency'>"+Dis[3]+"</span><span class='information-AI'></span></li>"+
                     "</ul>"+
                   "</li>"+
                 "</ul>"+
@@ -476,7 +499,7 @@ $(data).each(function (index, data) {$('.widgets').html(
                  "<ul class='ul-widgets'>"+
                   "<li><span class='accounting'>"+Instances[0]+"</span>"+
                     "<ul class='ul-infow'>"+
-                    "<li><span class='currency'>"+A1+"</span><span class='information-AI'></span></li>"+
+                    "<li><span class='currency'>"+Ava[0]+"</span><span class='information-AI'></span></li>"+
                     "</ul>"+
                   "</li>"+
                   "</ul>"+
@@ -486,7 +509,7 @@ $(data).each(function (index, data) {$('.widgets').html(
                  "<ul class='ul-widgets'>"+
                   "<li><span class='accounting'>"+Instances[1]+"</span>"+
                     "<ul class='ul-infow'>"+
-                    "<li><span class='currency'>"+A2+"</span><span class='information-AI'></span></li>"+
+                    "<li><span class='currency'>"+Ava[1]+"</span><span class='information-AI'></span></li>"+
                     "</ul>"+
                   "</li>"+
                 "</ul>"+
@@ -496,7 +519,7 @@ $(data).each(function (index, data) {$('.widgets').html(
                  "<ul class='ul-widgets'>"+
                   "<li><span class='accounting'>"+Instances[2]+"</span>"+
                     "<ul class='ul-infow'>"+
-                    "<li><span class='currency'>"+A3+"</span><span class='information-AI'></span></li>"+
+                    "<li><span class='currency'>"+Ava[2]+"</span><span class='information-AI'></span></li>"+
                     "</ul>"+
                   "</li>"+
                   "</ul>"+
@@ -506,7 +529,7 @@ $(data).each(function (index, data) {$('.widgets').html(
                  "<ul class='ul-widgets'>"+
                   "<li><span class='accounting'>"+Instances[3]+"</span>"+
                     "<ul class='ul-infow'>"+
-                    "<li><span class='currency'>"+A4+"</span><span class='information-AI'></span></li>"+
+                    "<li><span class='currency'>"+Ava[3]+"</span><span class='information-AI'></span></li>"+
                     "</ul>"+
                   "</li>"+
                 "</ul>"+
@@ -556,7 +579,7 @@ $(data).each(function (index, data) {$('.widgets').html(
                 "<ul class='ul-infow'>"+
                 "<li><span class='information-AA'>"+Interchange+"</span><span class='information-AI'>/AVG 1</span></li>"+
                 "</ul>"+
-              "</li>"+            
+              "</li>"+
             "</ul>"+
           "</div>"+
         "</div>"+
@@ -597,8 +620,8 @@ $(data).each(function (index, data) {$('.widgets').html(
         "<div class='bottom'>"+
         "<a href='trends_NAvailability.html'><span class='tendency-icon'></span></a>"+
             
-            "<div class='widget-information'>"+    
-                "<div class='col-lg-5 col-md-5 col-sm-5 col-xs-5  info-NAL'>"+
+            "<div class='widget-information'>"+
+              "<div class='col-lg-5 col-md-5 col-sm-5 col-xs-5  info-NAL'>"+
                  "<ul class='ul-widgets'>"+
                   "<li><span class='accounting'>CURRENT MONTH</span>"+
                     "<ul class='ul-infow'>"+
@@ -623,7 +646,7 @@ $(data).each(function (index, data) {$('.widgets').html(
         "</div>"
 );//Html
 }); //each
-
+for(var i=0, t=localStorage.length; i < t; i++) {
    key = localStorage.key(i);
    $("#"+data.widgets.working.id+"").addClass(""+localStorage[data.widgets.working.id]+"");
    $("#"+data.widgets.transactions.id+"").addClass(""+localStorage[data.widgets.transactions.id]+"");
@@ -643,8 +666,6 @@ $(".true").prepend();
 
 } //DataType
 });//Ajax end
-
-
 
 
 }); //End
